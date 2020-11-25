61dbd936208626d5d2ad081dd135053807e7ea3e
santy <santi.codev@gmail.com>
2020-11-25 14:04:56 +0100

    Front terminado

--- listadoArticulosFront/src/app/app.component.ts
+++ listadoArticulosFront/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ArticulosService } from './services/articulos.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listadoArticulosFront';
+  articulos=null;
+  
+  art={
+    codigo:null,
+    descripcion:null,
+    precio:null
+  }
+
+  constructor(private articulosServicio: ArticulosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.articulosServicio.recuperarTodos()
+      .subscribe(result => this.articulos = result);
+  }
+
+  alta() {
+    this.articulosServicio.alta(this.art)
+      .subscribe(datos => {
+        if (datos['resultado']=='OK') {
+          alert(datos['mensaje']);
+          this.recuperarTodos();
+        }
+      });
+  }
+
+  baja(codigo) {
+    this.articulosServicio.baja(codigo)
+      .subscribe(datos => {
+        if (datos['resultado']=='OK') {
+          alert(datos['mensaje']);
+          this.recuperarTodos();
+        }
+      });
+  }
+
+  modificacion() {
+    this.articulosServicio.modificacion(this.art)
+      .subscribe(datos => {
+        if (datos['resultado']=='OK') {
+          alert(datos['mensaje']);
+          this.recuperarTodos();
+        }
+      });    
+  }
+  
+  seleccionar(codigo) {
+    this.articulosServicio.seleccionar(codigo)
+      .subscribe(result => this.art = result[0]);
+  }
+
+  hayRegistros() {
+    return true;
+  } 
 }
